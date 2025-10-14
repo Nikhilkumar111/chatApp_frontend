5976b53a7ab5dfeaca6487c7ba19243525000231
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-10-14 18:49:46 +0530

    changes

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =  "https://chatapp-backendnew-2.onrender.com"
+const BASE_URL =  "https://chatapp-backendnew-3.onrender.com"
 
 const useAuthStore = create((set,get)=>({  
  authUser: null,
